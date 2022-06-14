8c9da0d6b2868826a76945aaa2d0be6de0633013
unforgivab1e <285182272@qq.com>
2022-06-14 17:17:57 +0800

    Build a five-by-five map

--- app/src/main/java/Nasa/java/app/Nasa.java
+++ app/src/main/java/Nasa/java/app/Nasa.java
@@ -1,9 +1,21 @@
 package main.java.Nasa.java.app;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Nasa {
     static String  DIRECTION_RULE = "NESW";
+    public static void main(String[] args) {
+        Nasa nasa=new Nasa();
+        List<Integer> location;
+        char direction;
+        Scanner sc = new Scanner(System.in);
+        String[] mapInput= sc.nextLine().split(" ");
+        nasa.getMap(Integer.parseInt(mapInput[0]),Integer.parseInt(mapInput[1]));
 
-    public static char turnAround(char direction, char action){
+    /*public static char turnAround(char direction, char action){
         int index= DIRECTION_RULE.lastIndexOf(direction);
         if((action+"").equalsIgnoreCase("R")){
             index++;
@@ -14,5 +26,11 @@ public class Nasa {
             index=index<0?index+4:index;
         }
         return DIRECTION_RULE.charAt(index);
-    }
+    }*/
+
 }
+    public List<Integer> getMap(int x, int y) {
+        return List.of(x,y);
+    }
+
+    }
--- app/src/test/java/Nasa/java/app/NasaTest.java
+++ app/src/test/java/Nasa/java/app/NasaTest.java
@@ -5,6 +5,10 @@ package test.java.Nasa.java.app;
 import main.java.Nasa.java.app.Nasa;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NasaTest {
@@ -16,13 +20,33 @@ class NasaTest {
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
     }
-
+    Nasa nasa=new Nasa();
     @Test
-    public void RotateAndActOnCommand(){
+    public void buildMap(){
+        int x=5;
+        int y=5;
+        assertEquals(5,nasa.getMap(x,y).get(0));
+        assertEquals(5,nasa.getMap(x,y).get(1));
+
+    }
+
+   /* @Test
+    public void RotateAndActFromCommand(){
         char direction='N';
         char action='R';
         assertEquals(Nasa.turnAround(direction,action),'E');
         char difAction='L';
         assertEquals(Nasa.turnAround(direction,difAction),'W');
-    }
+    }*/
+
+   /* @Test
+    public void GoOneDistanceForwardWhenMeetM(){
+        char direction='N';
+        List<Integer> location = List.of(1, 2);
+        List.of(5,5);
+
+        assertEquals(Nasa.turnAround(direction,action),'E');
+        char difAction='L';
+        assertEquals(Nasa.turnAround(direction,difAction),'W');
+    }*/
 }
