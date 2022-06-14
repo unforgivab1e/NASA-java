be84664bc0e75ff922d32e75f7ecd341d67038f0
unforgivab1e <285182272@qq.com>
2022-06-14 15:55:09 +0800

    turn left successful

---
+++ app/src/main/java/Nasa/java/app/Nasa.java
@@ -0,0 +1,19 @@
+package main.java.Nasa.java.app;
+
+public class Nasa {
+    static String  DIRECTION_RULE = "NESW";
+
+    public static char turnAround(char direction, char action){
+
+        int index= DIRECTION_RULE.lastIndexOf(direction);
+        if((action+"").equalsIgnoreCase("R")){
+            index++;
+            index=index>3?index-4:index;
+        }
+        if((action+"").equalsIgnoreCase("L")){
+            index--;
+            index=index<0?index+4:index;
+        }
+        return DIRECTION_RULE.charAt(index);
+    }
+}
---
+++ app/src/test/java/Nasa/java/app/NasaTest.java
@@ -0,0 +1,26 @@
+package test.java.Nasa.java.app;
+
+
+
+import main.java.Nasa.java.app.Nasa;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NasaTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void RotateAndActOnCommand(){
+        char direction='N';
+        char action='R';
+        assertEquals(Nasa.turnAround(direction,action),'E');
+    }
+}
