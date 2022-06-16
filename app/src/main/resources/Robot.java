df6968d5818b57b97d349ec80acea94a0193d0ba
unforgivab1e <285182272@qq.com>
2022-06-16 09:55:54 +0800

    The robot turns right once in the N direction

---
+++ app/src/main/resources/Robot.java
@@ -0,0 +1,46 @@
+package main.resources;
+
+public class Robot {
+    int x;
+    int y;
+    char direction;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+
+    public Robot(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+
+    public Robot turnRight() {
+        if (direction == 'N') {
+            direction = 'E';
+        }
+        return new Robot(x, y, direction);
+    }
+}
--- app/src/test/java/Nasa/java/app/NasaTest.java
+++ app/src/test/java/Nasa/java/app/NasaTest.java
@@ -2,6 +2,7 @@ package test.java.Nasa.java.app;
 
 
 import main.java.Nasa.java.app.Nasa;
+import main.resources.Robot;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -25,6 +26,12 @@ class NasaTest {
         List<Integer> map = nasa.buildMap(5, 5);
         assertEquals(5, map.get(0));
         assertEquals(5, map.get(1));
+    }
+
+    @Test
+    public void northTurnRightOneTime() {
+        Robot robot = new Robot(1, 2, 'N').turnRight();
+        assertEquals('E', robot.getDirection());
     }
 
 }
